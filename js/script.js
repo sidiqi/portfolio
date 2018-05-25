e059b4e77cd0dada0cfdc9ac7fe253c4eef286e5
Pavel <cssitemail@gmail.com>
2018-05-25 16:46:50 +0300

    scroll script changes

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ $(document).ready(function(){
 		//узнаем высоту от начала страницы до блока на который ссылается якорь
 			top = $(id).offset().top;
 		
-		//анимируем переход на расстояние - top за 1500 мс
-		$('body,html').animate({scrollTop: top}, 1500);
+		//анимируем переход на расстояние - top за 1600 мс
+		$('body,html').animate({scrollTop: top}, 1600);
 	});
 });
